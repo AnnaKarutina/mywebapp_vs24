5bb1aa724852764152fb14e348827180804545e9
Anna Karutina <anna.karutina@khk.ee>
2025-11-25 09:57:38 +0200

    Add delete user

--- src/main/java/com/mycompany/mywebapp/user/UserService.java
+++ src/main/java/com/mycompany/mywebapp/user/UserService.java
@@ -29,4 +29,11 @@ public class UserService {
         }
         throw new Exception("Could not find user with ID " + id);
     }
+
+    public void delete(Integer id) throws Exception {
+        if(id == null || id <= 0 || repo.findById(id).isEmpty()){
+            throw new Exception("Could not find user with ID " + id);
+        }
+        repo.deleteById(id);
+    }
 }
